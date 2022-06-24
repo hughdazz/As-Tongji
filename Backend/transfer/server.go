b166dca545e9b3f50fa3e93029f1671f130db539
hughdazz <hughdazz280@163.com>
2022-06-24 10:58:47 +0800

    debug

--- Backend/transfer/server.go
+++ Backend/transfer/server.go
@@ -86,6 +86,24 @@ func update_command() {
 	http.Get(url)
 }
 
+//跨域访问：cross  origin resource share
+func CrosHandler() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		// method := context.Request.Method
+		context.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		context.Header("Access-Control-Allow-Origin", "*") // 设置允许访问所有域
+		context.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE")
+		context.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session,X_Requested_With,Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Pragma,token,openid,opentoken")
+		context.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers,Cache-Control,Content-Language,Content-Type,Expires,Last-Modified,Pragma,FooBar")
+		context.Header("Access-Control-Max-Age", "172800")
+		context.Header("Access-Control-Allow-Credentials", "false")
+		context.Set("content-type", "application/json") //设置返回格式是json
+
+		//处理请求
+		context.Next()
+	}
+}
+
 // 私人信息组
 
 // 公共信息组
@@ -107,10 +125,14 @@ func main() {
 	public := router.Group("/public")
 	private := router.Group("/private")
 
+	//设置响应头
+	router.Use(CrosHandler())
+
 	// 全局连接服务器
 	go connServer.run()
 	router.GET("/init", func(c *gin.Context) {
 		update_command()
+
 		c.JSON(http.StatusOK, gin.H{"msg": "ok"})
 	})
 	private.POST("/", func(c *gin.Context) {
